37ac076db8700dc6b322faf466617abd53990b2a
Kranti Kumar Shah <Krantikumar803@gmail.com>
2023-02-05 12:03:06 +0545

    final final commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import {config} from 'dotenv';
 import ErrorMiddleware  from './middlewares/Error.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 config({
     path:'./config/config.env'
@@ -19,6 +20,12 @@ app.use(
 )
 app.use(cookieParser());
 
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:['GET','POST','PUT','DELETE']
+}))
+
 //importing and using routes
 import blogRoutes from './routes/blogRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -31,5 +38,9 @@ app.use('/api/v1',adminRoutes);
 
 
 export default app;
+
+app.get('/',(req,res)=>{
+    res.send(`<h1>Site is working on <a href=${process.env.FRONTEND_URL}></a> `)
+})
 
 app.use(ErrorMiddleware);
--- utils/sendToken.js
+++ utils/sendToken.js
@@ -5,7 +5,7 @@ export const sendToken = (res,user,statusCode=200,message) => {
     const options = {
         expires:new Date(Date.now()+15*24*60*60*1000),
         httpOnly:true,
-        // secure:true,
+        secure:true,
         sameSite:'none',
     }
 
