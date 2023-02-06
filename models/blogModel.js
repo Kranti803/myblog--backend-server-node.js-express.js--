d592ca45f39ffd46effaa6f060b95d4bfe90d8e2
Kranti Kumar Shah <Krantikumar803@gmail.com>
2023-02-06 14:57:22 +0545

    fixed blog category

--- models/blogModel.js
+++ models/blogModel.js
@@ -23,7 +23,7 @@ const blogSchema = new mongoose.Schema({
     category: {
         type: String,
         required: [true, "Please Select Product Category"],
-        
+        enum:["Adventure","Travel","Fashion","Technology"]
 
     },
     featured: {
