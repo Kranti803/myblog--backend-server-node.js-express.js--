8136b1cd9eb5bfb4f73c1c7717293e5a1200bbaf
Kranti Kumar Shah <Krantikumar803@gmail.com>
2023-02-07 13:57:43 +0545

    fixed deleteUser feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,6 +3,7 @@ import { ErrorHandler } from './../utils/ErrorHandler.js';
 import { User } from './../models/userModel.js';
 import { Blog } from './../models/blogModel.js';
 import { sendEmail } from './../utils/sendEmail.js';
+import cloudinary from 'cloudinary'
 
 
 
@@ -24,7 +25,7 @@ export const getAllUsers = catchAsyncError(async (req, res, next) => {
 
 export const contactUs = catchAsyncError(async (req, res, next) => {
 
-    const { name, email,subject, message } = req.body;
+    const { name, email, subject, message } = req.body;
 
     if (!name || !email || !subject || !message) return next(new ErrorHandler('Please fill the field'), 400)
 
@@ -46,40 +47,40 @@ export const contactUs = catchAsyncError(async (req, res, next) => {
 // change role...
 export const changeRole = catchAsyncError(async (req, res, next) => {
 
-    
+
     const user = await User.findById(req.query.id);
 
-    if(!user) return next(new ErrorHandler('User doesnot exists'));
+    if (!user) return next(new ErrorHandler('User doesnot exists'));
 
-    
+
     if (user.role === 'user') user.role = 'admin';
     else user.role = 'user';
-    
+
     await user.save();
 
     res.status(200).json({
         success: true,
-        message:"Role changed successfully",
+        message: "Role changed successfully",
     })
 
 })
 
 export const deleteUser = catchAsyncError(async (req, res, next) => {
 
-    
+
     const user = await User.findById(req.query.id);
 
-    if(!user) return next(new ErrorHandler('User doesnot exists'));
+    if (!user) return next(new ErrorHandler('User doesnot exists'));
 
     await cloudinary.v2.uploader.destroy(user.avtar.public_id);
 
     await User.findByIdAndDelete(req.query.id);
-    
+
     await User.save();
 
     res.status(200).json({
         success: true,
-        message:"User deleted successfully",
+        message: "User deleted successfully",
     })
 
 })
@@ -88,21 +89,21 @@ export const deleteUser = catchAsyncError(async (req, res, next) => {
 // change blog featured type...
 export const changeFeatured = catchAsyncError(async (req, res, next) => {
 
-    
+
     const blog = await Blog.findById(req.query.id);
 
-    if(!blog) return next(new ErrorHandler('Blog doesnot exists'));
+    if (!blog) return next(new ErrorHandler('Blog doesnot exists'));
 
-    
+
     if (blog.featured) blog.featured = false;
-    
+
     else blog.featured = true;
-    
+
     await blog.save();
 
     res.status(200).json({
         success: true,
-        message:"Blog featured changed successfully",
+        message: "Blog featured changed successfully",
     })
 
 })
